1c2bbfeb299bfe7d14baef903b9ecdb22bf7ed18
Jmendes2022 <jmendes2022@gmail.com>
2026-01-27 20:29:03 -0500

    worked on course

--- 02-Fundamentals-Part-2/starter/assignment.js
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -1,5 +1,6 @@
 "use strict";
 
+/*
 const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
 const avgScoreDolphins1 = calcAverage(44, 23, 71);
@@ -20,3 +21,21 @@ function checkWinner(avgScoreDolphins, avgScoreKoalas) {
 
 checkWinner(avgScoreDolphins1, avgScoreKoalas1);
 checkWinner(avgScoreDolphins2, avgScoreKoalas2);
+
+
+const fullName = (firstName, lastName) => `${firstName} ${lastName}`;
+
+const firstName = "Jordan";
+const lastName = "Mendes";
+
+console.log(fullName(firstName, lastName));
+
+const calcTip = (bill) =>
+  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [tips[0] + 125, tips[1] + 555, tips[2] + 44];
+
+console.log(bills, tips, totals);
+*/
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -75,7 +75,6 @@ function fruitProcessor(apples, oranges) {
 
 console.log(fruitProcessor(2, 3));
 
-  */
 
 const calcAge = function (birthYear) {
   return 2037 - birthYear;
@@ -84,7 +83,7 @@ const calcAge = function (birthYear) {
 const yearsUntilRetirement = function (birthYear, firstName) {
   const age = calcAge(birthYear);
   const retirement = 67 - age;
-
+  
   if (retirement > 0) {
     console.log(`${firstName} retires in ${retirement} years`);
     return retirement;
@@ -96,3 +95,76 @@ const yearsUntilRetirement = function (birthYear, firstName) {
 
 console.log(yearsUntilRetirement(1991, "Jordan"));
 console.log(yearsUntilRetirement(1950, "Victor"));
+
+
+const friend1 = "Michael";
+const friend2 = "Steven";
+const friend3 = "Peter";
+
+const friends = ["MIchael", "Steven", "Peter"];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Jay";
+console.log(friends);
+
+const firstName = "Jordan";
+
+const jordan = [firstName, "Mendes", 2037 - 1991, "teacher", friends];
+
+console.log(jordan);
+
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1]),
+];
+
+console.log(ages);
+
+*/
+
+const friends = ["Michael", "Steven", "Peter"];
+let newLength = friends.push("Jay");
+console.log(friends);
+console.log(newLength);
+
+friends.unshift("John");
+console.log(friends);
+
+friends.pop();
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift();
+console.log(friends);
+
+console.log(friends.indexOf("Steven"));
+console.log(friends.indexOf("Bob"));
+
+friends.push(23);
+console.log(friends.includes("Steven"));
+console.log(friends.includes("Bob"));
+console.log(friends.includes("23"));
+
+if (friends.includes("Steven")) {
+  console.log("You have a friend called Steven");
+}
